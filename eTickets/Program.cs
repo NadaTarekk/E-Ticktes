dc0b14ac2dab768518b60347338f1b14c1f8ad7f
NadaTarekk <nadat1429@gmail.com>
2024-04-24 01:52:59 +0200

    Add connection string

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(optionBuilder => {
-    optionBuilder.UseSqlServer();
+    optionBuilder.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
 });
 
 var app = builder.Build();
